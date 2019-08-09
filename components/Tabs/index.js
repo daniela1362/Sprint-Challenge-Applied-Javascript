5abe1ed4708489573dbb16bef38e4530a22c00e8
daniela-arias-ordonez <daniela.arias2000@gmail.com>
2019-08-09 12:23:15 -0500

    New changes

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,32 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const topic = document.querySelector('.topic');
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+.then(data =>{
+    console.log('response', data)
+    const topicData= data.data;
+    const topic = document.querySelector('.topic';
+    topic.appendChild(topicData));
+})
+.catch( err => {
+    console.log('Error retrieving data',err)
+});
+
+function newTabs(object){
+    const tab = document.createElement('div');
+    const topic = document.createElement('div');
+    const title = document.createElement('span');
+}
+
+tab.appendChild(topic);
+tab.appendChild(title);
+
+tab.classList.add('tab, topic');
+topic.classList.add('title')
+
+title.textContent = object.topic;
+return tab;
+}
